6f611fc5921f05a4da2ee238f19eb95a90559af0
erictragoustis <athenscatsgr@gmail.com>
2025-03-17 23:44:06 +0200

    Added basic models

--- vuvoregs/event/admin.py
+++ vuvoregs/event/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Event, RaceType, Race, RacePackages
 
 # Register your models here.
+
+admin.site.register(Event)
+admin.site.register(RaceType)
+admin.site.register(Race)
+admin.site.register(RacePackages)
--- vuvoregs/event/models.py
+++ vuvoregs/event/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField()
+    location = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/event_images/')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+class RaceType(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Race(models.Model):
+    title = models.CharField(max_length=100)
+    type = models.ForeignKey(RaceType, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    distance = models.FloatField()
+    start_time = models.TimeField()
+    image = models.ImageField(upload_to='images/race_images/')
+    active = models.BooleanField(default=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+class RacePackages(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    price = models.FloatField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
